790b949bd624f4411354294f2ebadff6bc0c0087
asafpamzn <97948347+asafpamzn@users.noreply.github.com>
2025-05-23 18:36:25 +0300

    stam

--- redis_new12.c
+++ redis_new12.c
@@ -672,15 +672,65 @@ PHP_METHOD(Redis, sInterStore)
     redis_object *redis;
     char *dst = NULL;
     size_t dst_len;
-    zval *z_args;
+    zval *z_args = NULL;
     int argc = 0;
+    zval *z_keys_arr = NULL;
+    HashTable *ht_keys = NULL;
+    int keys_count = 0;
+    zval *z_extracted_keys = NULL;
 
-    /* Parse parameters */
-    if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os+",
-                                     &object, redis_ce, &dst, &dst_len,
-                                     &z_args, &argc) == FAILURE)
+    /* First argument is always the destination key */
+    if (zend_parse_method_parameters(1, getThis(), "Os",
+                                     &object, redis_ce, &dst, &dst_len) == FAILURE)
     {
         RETURN_FALSE;
+    }
+
+    /* Check if we have exactly two parameters (destination + array) */
+    if (ZEND_NUM_ARGS() == 2)
+    {
+        /* Try to parse second parameter as an array */
+        if (zend_parse_parameters(1, "a", &z_keys_arr) == SUCCESS)
+        {
+            /* We have an array of keys */
+            ht_keys = Z_ARRVAL_P(z_keys_arr);
+            keys_count = zend_hash_num_elements(ht_keys);
+
+            /* If array is empty, return FALSE */
+            if (keys_count == 0)
+            {
+                RETURN_FALSE;
+            }
+
+            /* Allocate memory for array of zvals */
+            z_extracted_keys = ecalloc(keys_count, sizeof(zval));
+
+            /* Copy array values to sequential array */
+            zval *data;
+            int idx = 0;
+            ZEND_HASH_FOREACH_VAL(ht_keys, data)
+            {
+                ZVAL_COPY(&z_extracted_keys[idx], data);
+                idx++;
+            }
+            ZEND_HASH_FOREACH_END();
+
+            /* Set for later use */
+            z_args = z_extracted_keys;
+            argc = keys_count;
+        }
+    }
+
+    /* If we didn't get an array, parse remaining args as variadic */
+    if (!z_args)
+    {
+        /* Parse all parameters including destination key */
+        if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os+",
+                                         &object, redis_ce, &dst, &dst_len,
+                                         &z_args, &argc) == FAILURE)
+        {
+            RETURN_FALSE;
+        }
     }
 
     /* Get Redis object */
@@ -694,13 +744,43 @@ PHP_METHOD(Redis, sInterStore)
         if (execute_sinterstore_command(redis->glide_client, dst, dst_len,
                                         z_args, argc, &result_value))
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             /* Return the cardinality of the resulting set */
             RETURN_LONG(result_value);
         }
         else
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             RETURN_FALSE;
         }
+    }
+
+    /* Clean up if we allocated memory for the array keys but didn't execute the command */
+    if (z_extracted_keys)
+    {
+        for (int i = 0; i < keys_count; i++)
+        {
+            zval_dtor(&z_extracted_keys[i]);
+        }
+        efree(z_extracted_keys);
     }
 }
 /* }}} */
@@ -710,14 +790,57 @@ PHP_METHOD(Redis, sUnion)
 {
     zval *object;
     redis_object *redis;
-    zval *z_args;
+    zval *z_args = NULL;
     int argc = 0;
+    zval *z_keys_arr = NULL;
+    HashTable *ht_keys = NULL;
+    int keys_count = 0;
+    zval *z_extracted_keys = NULL;
 
-    /* Parse parameters */
-    if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "O+",
-                                     &object, redis_ce, &z_args, &argc) == FAILURE)
+    /* Check if we have a single array argument or variadic string arguments */
+    if (ZEND_NUM_ARGS() == 1)
     {
-        RETURN_FALSE;
+        /* Try to parse as a single array argument */
+        if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Oa",
+                                         &object, redis_ce, &z_keys_arr) == SUCCESS)
+        {
+            /* We have an array of keys */
+            ht_keys = Z_ARRVAL_P(z_keys_arr);
+            keys_count = zend_hash_num_elements(ht_keys);
+
+            /* If array is empty, return FALSE */
+            if (keys_count == 0)
+            {
+                RETURN_FALSE;
+            }
+
+            /* Allocate memory for array of zvals */
+            z_extracted_keys = ecalloc(keys_count, sizeof(zval));
+
+            /* Copy array values to sequential array */
+            zval *data;
+            int idx = 0;
+            ZEND_HASH_FOREACH_VAL(ht_keys, data)
+            {
+                ZVAL_COPY(&z_extracted_keys[idx], data);
+                idx++;
+            }
+            ZEND_HASH_FOREACH_END();
+
+            /* Set for later use */
+            z_args = z_extracted_keys;
+            argc = keys_count;
+        }
+    }
+
+    /* If we didn't get an array, parse as variadic arguments */
+    if (!z_args)
+    {
+        if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "O+",
+                                         &object, redis_ce, &z_args, &argc) == FAILURE)
+        {
+            RETURN_FALSE;
+        }
     }
 
     /* Get Redis object */
@@ -730,13 +853,43 @@ PHP_METHOD(Redis, sUnion)
         if (execute_sunion_command(redis->glide_client, z_args, argc,
                                    return_value))
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             /* Return value already set in execute_sunion_command */
             return;
         }
         else
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             RETURN_FALSE;
         }
+    }
+
+    /* Clean up if we allocated memory for the array keys but didn't execute the command */
+    if (z_extracted_keys)
+    {
+        for (int i = 0; i < keys_count; i++)
+        {
+            zval_dtor(&z_extracted_keys[i]);
+        }
+        efree(z_extracted_keys);
     }
 }
 /* }}} */
@@ -748,15 +901,65 @@ PHP_METHOD(Redis, sUnionStore)
     redis_object *redis;
     char *dst = NULL;
     size_t dst_len;
-    zval *z_args;
+    zval *z_args = NULL;
     int argc = 0;
+    zval *z_keys_arr = NULL;
+    HashTable *ht_keys = NULL;
+    int keys_count = 0;
+    zval *z_extracted_keys = NULL;
 
-    /* Parse parameters */
-    if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os+",
-                                     &object, redis_ce, &dst, &dst_len,
-                                     &z_args, &argc) == FAILURE)
+    /* First argument is always the destination key */
+    if (zend_parse_method_parameters(1, getThis(), "Os",
+                                     &object, redis_ce, &dst, &dst_len) == FAILURE)
     {
         RETURN_FALSE;
+    }
+
+    /* Check if we have exactly two parameters (destination + array) */
+    if (ZEND_NUM_ARGS() == 2)
+    {
+        /* Try to parse second parameter as an array */
+        if (zend_parse_parameters(1, "a", &z_keys_arr) == SUCCESS)
+        {
+            /* We have an array of keys */
+            ht_keys = Z_ARRVAL_P(z_keys_arr);
+            keys_count = zend_hash_num_elements(ht_keys);
+
+            /* If array is empty, return FALSE */
+            if (keys_count == 0)
+            {
+                RETURN_FALSE;
+            }
+
+            /* Allocate memory for array of zvals */
+            z_extracted_keys = ecalloc(keys_count, sizeof(zval));
+
+            /* Copy array values to sequential array */
+            zval *data;
+            int idx = 0;
+            ZEND_HASH_FOREACH_VAL(ht_keys, data)
+            {
+                ZVAL_COPY(&z_extracted_keys[idx], data);
+                idx++;
+            }
+            ZEND_HASH_FOREACH_END();
+
+            /* Set for later use */
+            z_args = z_extracted_keys;
+            argc = keys_count;
+        }
+    }
+
+    /* If we didn't get an array, parse remaining args as variadic */
+    if (!z_args)
+    {
+        /* Parse all parameters including destination key */
+        if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os+",
+                                         &object, redis_ce, &dst, &dst_len,
+                                         &z_args, &argc) == FAILURE)
+        {
+            RETURN_FALSE;
+        }
     }
 
     /* Get Redis object */
@@ -770,13 +973,43 @@ PHP_METHOD(Redis, sUnionStore)
         if (execute_sunionstore_command(redis->glide_client, dst, dst_len,
                                         z_args, argc, &result_value))
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             /* Return the cardinality of the resulting set */
             RETURN_LONG(result_value);
         }
         else
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             RETURN_FALSE;
         }
+    }
+
+    /* Clean up if we allocated memory for the array keys but didn't execute the command */
+    if (z_extracted_keys)
+    {
+        for (int i = 0; i < keys_count; i++)
+        {
+            zval_dtor(&z_extracted_keys[i]);
+        }
+        efree(z_extracted_keys);
     }
 }
 /* }}} */
@@ -786,14 +1019,57 @@ PHP_METHOD(Redis, sDiff)
 {
     zval *object;
     redis_object *redis;
-    zval *z_args;
+    zval *z_args = NULL;
     int argc = 0;
+    zval *z_keys_arr = NULL;
+    HashTable *ht_keys = NULL;
+    int keys_count = 0;
+    zval *z_extracted_keys = NULL;
 
-    /* Parse parameters */
-    if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "O+",
-                                     &object, redis_ce, &z_args, &argc) == FAILURE)
+    /* Check if we have a single array argument or variadic string arguments */
+    if (ZEND_NUM_ARGS() == 1)
     {
-        RETURN_FALSE;
+        /* Try to parse as a single array argument */
+        if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Oa",
+                                         &object, redis_ce, &z_keys_arr) == SUCCESS)
+        {
+            /* We have an array of keys */
+            ht_keys = Z_ARRVAL_P(z_keys_arr);
+            keys_count = zend_hash_num_elements(ht_keys);
+
+            /* If array is empty, return FALSE */
+            if (keys_count == 0)
+            {
+                RETURN_FALSE;
+            }
+
+            /* Allocate memory for array of zvals */
+            z_extracted_keys = ecalloc(keys_count, sizeof(zval));
+
+            /* Copy array values to sequential array */
+            zval *data;
+            int idx = 0;
+            ZEND_HASH_FOREACH_VAL(ht_keys, data)
+            {
+                ZVAL_COPY(&z_extracted_keys[idx], data);
+                idx++;
+            }
+            ZEND_HASH_FOREACH_END();
+
+            /* Set for later use */
+            z_args = z_extracted_keys;
+            argc = keys_count;
+        }
+    }
+
+    /* If we didn't get an array, parse as variadic arguments */
+    if (!z_args)
+    {
+        if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "O+",
+                                         &object, redis_ce, &z_args, &argc) == FAILURE)
+        {
+            RETURN_FALSE;
+        }
     }
 
     /* Get Redis object */
@@ -806,13 +1082,43 @@ PHP_METHOD(Redis, sDiff)
         if (execute_sdiff_command(redis->glide_client, z_args, argc,
                                   return_value))
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             /* Return value already set in execute_sdiff_command */
             return;
         }
         else
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             RETURN_FALSE;
         }
+    }
+
+    /* Clean up if we allocated memory for the array keys but didn't execute the command */
+    if (z_extracted_keys)
+    {
+        for (int i = 0; i < keys_count; i++)
+        {
+            zval_dtor(&z_extracted_keys[i]);
+        }
+        efree(z_extracted_keys);
     }
 }
 /* }}} */
@@ -824,15 +1130,65 @@ PHP_METHOD(Redis, sDiffStore)
     redis_object *redis;
     char *dst = NULL;
     size_t dst_len;
-    zval *z_args;
+    zval *z_args = NULL;
     int argc = 0;
+    zval *z_keys_arr = NULL;
+    HashTable *ht_keys = NULL;
+    int keys_count = 0;
+    zval *z_extracted_keys = NULL;
 
-    /* Parse parameters */
-    if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os+",
-                                     &object, redis_ce, &dst, &dst_len,
-                                     &z_args, &argc) == FAILURE)
+    /* First argument is always the destination key */
+    if (zend_parse_method_parameters(1, getThis(), "Os",
+                                     &object, redis_ce, &dst, &dst_len) == FAILURE)
     {
         RETURN_FALSE;
+    }
+
+    /* Check if we have exactly two parameters (destination + array) */
+    if (ZEND_NUM_ARGS() == 2)
+    {
+        /* Try to parse second parameter as an array */
+        if (zend_parse_parameters(1, "a", &z_keys_arr) == SUCCESS)
+        {
+            /* We have an array of keys */
+            ht_keys = Z_ARRVAL_P(z_keys_arr);
+            keys_count = zend_hash_num_elements(ht_keys);
+
+            /* If array is empty, return FALSE */
+            if (keys_count == 0)
+            {
+                RETURN_FALSE;
+            }
+
+            /* Allocate memory for array of zvals */
+            z_extracted_keys = ecalloc(keys_count, sizeof(zval));
+
+            /* Copy array values to sequential array */
+            zval *data;
+            int idx = 0;
+            ZEND_HASH_FOREACH_VAL(ht_keys, data)
+            {
+                ZVAL_COPY(&z_extracted_keys[idx], data);
+                idx++;
+            }
+            ZEND_HASH_FOREACH_END();
+
+            /* Set for later use */
+            z_args = z_extracted_keys;
+            argc = keys_count;
+        }
+    }
+
+    /* If we didn't get an array, parse remaining args as variadic */
+    if (!z_args)
+    {
+        /* Parse all parameters including destination key */
+        if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os+",
+                                         &object, redis_ce, &dst, &dst_len,
+                                         &z_args, &argc) == FAILURE)
+        {
+            RETURN_FALSE;
+        }
     }
 
     /* Get Redis object */
@@ -846,13 +1202,43 @@ PHP_METHOD(Redis, sDiffStore)
         if (execute_sdiffstore_command(redis->glide_client, dst, dst_len,
                                        z_args, argc, &result_value))
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             /* Return the cardinality of the resulting set */
             RETURN_LONG(result_value);
         }
         else
         {
+            /* Clean up if we allocated memory for the array keys */
+            if (z_extracted_keys)
+            {
+                for (int i = 0; i < keys_count; i++)
+                {
+                    zval_dtor(&z_extracted_keys[i]);
+                }
+                efree(z_extracted_keys);
+            }
+
             RETURN_FALSE;
         }
+    }
+
+    /* Clean up if we allocated memory for the array keys but didn't execute the command */
+    if (z_extracted_keys)
+    {
+        for (int i = 0; i < keys_count; i++)
+        {
+            zval_dtor(&z_extracted_keys[i]);
+        }
+        efree(z_extracted_keys);
     }
 }
 /* }}} */
